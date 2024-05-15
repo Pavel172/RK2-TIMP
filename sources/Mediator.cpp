47e6ff6df8e9bcd1ff40f6d2e8bfd23bca9b96a5
Pavel172 <pascha.lustin@yandex.ru>
2024-05-15 17:18:34 +0300

    added CMakeLists.txt

--- sources/Mediator.cpp
+++ sources/Mediator.cpp
@@ -116,21 +116,4 @@ void ConcreteColleague::send( std::string msg )
 }
 
 
-int main()
-{
-  Mediator *mediator = new ConcreteMediator();
-  
-  Colleague *c1 = new ConcreteColleague( mediator, 1 );
-  Colleague *c2 = new ConcreteColleague( mediator, 2 );
-  Colleague *c3 = new ConcreteColleague( mediator, 3 );
-  
-  mediator->add( c1 );
-  mediator->add( c2 );
-  mediator->add( c3 );
-  
-  c1->send( "Hi!" );
-  c3->send( "Hello!" );
-  
-  delete mediator;
-  return 0;
-}
+
